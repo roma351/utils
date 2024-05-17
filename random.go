5ad4fd8aa6fe6956ad72ec01608cff465c2c5afe
acer 7-8 <acer@MacBook-Pro-acer.local>
2024-05-17 14:11:56 +0300

    add func randint2 with crypto/rand

--- random.go
+++ random.go
@@ -1,11 +1,28 @@
 package utils
 
-import "math/rand"
+import (
+	rand2 "crypto/rand"
+	"log"
+	"math/big"
+	"math/rand"
+)
 
-// -> crypto/rand
 func RandInt(min int, max int) int {
 	if min == max {
 		return min
 	}
 	return rand.Intn(max-min) + min
+}
+
+func RandInt2(min int, max int) int {
+	if min == max {
+		return min
+	}
+	n := max - min
+	randInt, err := rand2.Int(rand2.Reader, big.NewInt(int64(n)))
+	if err != nil {
+		log.Fatalln("Error generating random number:", err)
+	}
+
+	return int(randInt.Int64()) + min
 }
