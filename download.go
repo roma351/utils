4e0b5ca425ef52f32a8a51f61762c95f52f16422
acer 7-8 <acer@MacBook-Pro-acer.local>
2024-08-21 19:22:49 +0300

    a

--- download.go
+++ download.go
@@ -1,1 +1,21 @@
 package utils
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+)
+
+func DownloadImageAsByte(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), err
+}
